27b718f4a8a8c7d28c138e32e67989609a552f1b
Evgeny <inkermankirov@gmail.com>
2026-09-11 00:02:35 +0300

    Load calculator history in currency converter

--- header-currency-display.js
+++ header-currency-display.js
@@ -83,3 +83,11 @@
   }
   start();
 })();
+
+(() => {
+  if(document.querySelector('script[data-currency-calculator-history]')) return;
+  const s=document.createElement('script');
+  s.src='currency-calculator-history.js';
+  s.dataset.currencyCalculatorHistory='1';
+  document.body.appendChild(s);
+})();
