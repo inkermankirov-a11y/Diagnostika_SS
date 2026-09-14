87f7edd4910d7a3e654f75b03b6f465bae8f6e05
Evgeny <inkermankirov@gmail.com>
2026-09-14 13:10:03 +0300

    Add Google OAuth client configuration

---
+++ google-oauth-config.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Web OAuth Client ID for Google Identity Services.
+// This value is public by design and may be committed to the frontend.
+// Configure Authorized JavaScript origin in Google Cloud:
+// https://inkermankirov-a11y.github.io
+window.DIAGNOSTIKA_GOOGLE_CLIENT_ID='';
